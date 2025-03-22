29fb4ce475542b25ddd3fbaee095445fab361602
gopi744 <gn130324@gmail.com>
2025-03-22 04:39:22 +0000

    MGV

--- src/app/home/constants.ts
+++ src/app/home/constants.ts
@@ -9,7 +9,7 @@ import { deepClone } from "lib/deep-clone";
 
 export const END_HOME_RESUME: Resume = {
   profile: {
-    name: "GOPI.M",
+    name: "MGV",
     summary:
       "Software engineer obsessed with building exceptional products that people love",
     email: "hello@openresume.com",
